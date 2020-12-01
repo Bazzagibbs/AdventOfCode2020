75093e920891e8f3d1cbb83c5d11b89ed3d992eb
Bazzagibbs <bailey.gibbons2018@gmail.com>
2020-12-01 23:41:47 +1100

    cleaned up day1

--- day1/1.cpp
+++ day1/1.cpp
@@ -1,7 +1,5 @@
 #include <iostream>
-#include <string>
 #include <vector>
-#include <stdlib.h>
 
 using namespace std;
 
@@ -14,15 +12,15 @@ int main(){
        v.push_back(y); 
     }
 
+    // O(n^3) solution
     for(int i = 0; i < v.size()-2; i++){
         for(int j = i+1; j < v.size()-1; j++){
-            for(int k = j+1; k < v.size(); k++)
-            if(v[i] + v[j] + v[k] == 2020){
-                cout << v[i] * v[j] * v[k];
+            for(int k = j+1; k < v.size(); k++){
+                if(v[i] + v[j] + v[k] == 2020){
+                    cout << v[i] * v[j] * v[k];
+                }
             }
         }
     } 
-
     return 0;
-
 }
